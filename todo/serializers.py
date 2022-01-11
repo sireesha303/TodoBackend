8ec1532ee22489db9a7dec574e040fa8ab9714e5
sireesha <msireesha30031999@gmail.com>
2022-01-11 17:26:49 +0530

    adding serializers file and app url

---
+++ todo/serializers.py
@@ -0,0 +1,15 @@
+"""
+Serializers for todoapp models
+"""
+
+from rest_framework import serializers
+from .models import *
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title',
+        )
+        model = Todo
