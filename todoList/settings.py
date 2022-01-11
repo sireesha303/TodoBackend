bc4a0fc9dc328fbc8c271983739e6b57fa15bb2d
sireesha <msireesha30031999@gmail.com>
2022-01-11 17:27:23 +0530

    adding views and urls files

--- todoList/settings.py
+++ todoList/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
